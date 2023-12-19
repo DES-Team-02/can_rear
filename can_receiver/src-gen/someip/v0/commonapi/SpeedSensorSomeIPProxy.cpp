649e7e1de462d3c1aaa928e666189ec7448647ff
Wasabi <55065075+kianwasabi@users.noreply.github.com>
2023-12-19 17:51:16 +0000

    Auto Generated fidl

--- can_receiver/src-gen/someip/v0/commonapi/SpeedSensorSomeIPProxy.cpp
+++ can_receiver/src-gen/someip/v0/commonapi/SpeedSensorSomeIPProxy.cpp
@@ -32,7 +32,7 @@ std::shared_ptr<CommonAPI::SomeIP::Proxy> createSpeedSensorSomeIPProxy(
 
 void initializeSpeedSensorSomeIPProxy() {
     CommonAPI::SomeIP::AddressTranslator::get()->insert(
-        "local:commonapi.SpeedSensor:v0_1:SpeedSensor",
+        "local:commonapi.SpeedSensor:v0_1:commonapi.SpeedSensor",
         0x3000, 0x55f0, 0, 1);
     CommonAPI::SomeIP::Factory::get()->registerProxyCreateMethod(
         "commonapi.SpeedSensor:v0_1",
